d5480897c99346cc9232b94908c7dbddbdcf8ecd
Andrei Maksimov <maksimov.andrei@gmail.com>
2021-10-19 14:35:17 -0400

    Fix buildspec-checkov.yml

--- main.tf
+++ main.tf
@@ -46,6 +46,8 @@ resource "aws_s3_bucket_public_access_block" "s3Public_test" {
   bucket                  = aws_s3_bucket.test.id
   block_public_acls       = true
   block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "test" {
